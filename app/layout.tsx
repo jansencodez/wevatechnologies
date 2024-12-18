f882641a638a6258d0fd13fa85c77f2877bd29a2
jansencodez <nyamwecliff@gmail.com>
2024-12-18 14:40:34 +0300

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,51 @@
-import type { Metadata } from "next";
-
+import { Metadata } from "next";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
-
-import AnimatedLayout from "@/components/AnimatedLayouts"; // Import the AnimatedLayout
+import AnimatedLayout from "@/components/AnimatedLayouts";
 import Footer from "@/components/Footer";
 
-//contexts
+// Contexts
 import { BlogsProvider } from "@/context/BlogsContext";
 import { ServicesProvider } from "@/context/servicesContext";
 import { EventsProvider } from "@/context/EventContext";
 import { InsightsProvider } from "@/context/InsightsContext";
 import { AnnouncementsProvider } from "@/context/AnnouncementsContext";
 
+// Define the metadata with the correct types for SEO optimization
 export const metadata: Metadata = {
-  title: "Weva Tech",
+  title: "Weva Technologies",
   description: "Innovative solutions in AgriTech, EdTech, and more.",
+  keywords:
+    "Wevatechnologies, Weva Tech, Weva, Weva Technologies, AgriTech, EdTech, Weva Tech, technology solutions, innovation, agriculture, education",
+  openGraph: {
+    type: "website",
+    title: "Weva Technologies",
+    description: "Innovative solutions in AgriTech, EdTech, and more.",
+    url: "https://wevatechnologies.vercel.app",
+    siteName: "Weva Technologies",
+    locale: "en_KE",
+    images: [
+      {
+        url: "/images/logo.png",
+        width: 800,
+        height: 600,
+        alt: "Weva Tech Logo",
+      },
+    ],
+  },
+  robots: "index, follow", // Default robots setting for most pages
+  viewport: "width=device-width, initial-scale=1.0",
 };
 
-function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
+      <head>
+        <title>Weva Technologies</title>
+        <link rel="canonical" href="https://wevatechnologies.vercel.app" />
+
+        {/* No need to manually add meta tags, metadata will be applied automatically */}
+      </head>
       <body>
         {/* Header */}
         <header className="bg-gradient-to-r from-gray-100 via-gray-200 to-gray-300 text-gray-900 shadow-md sticky top-0 z-50 border-b-2 border-gray-400">
@@ -44,7 +65,7 @@ function RootLayout({
       </body>
     </html>
   );
-}
+};
 
 const ContexedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
